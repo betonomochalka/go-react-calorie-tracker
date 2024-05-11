02cf31e4d3d4c3942dd0353a1d03e4e0e2309263
betonomochalka <zxcnobrainn@gmail.com>
2024-05-11 13:45:34 +0200

    Dont remember

--- server/main.go
+++ server/main.go
@@ -20,7 +20,7 @@ func main() {
 
 	router.POST("/entry/create", routes.AddEntry)
 	router.GET("/entries", routes.GetEntries)
-	router.GET("/entries/:id/", routes.GetEntriesById)
+	router.GET("/entries/:id/", routes.GetEntryById)
 	router.GET("/ingredient/:ingredient", routes.GetEntriesByIngredient)
 
 	router.PUT("/entry/update/:id", routes.UpdateEntry)
---
+++ server/routes/entries.go
@@ -0,0 +1,88 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
+
+func AddEntry(c *gin.Context) {
+
+}
+
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entries []bson.M
+	cusrsor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cusrsor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
+
+func GetEntryById(c *gin.Context) {
+	EntryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(EntryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry bson.M
+	if err := entryCollection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	fmt.Println(entry)
+	c.JSON(http.StatusOK, entry)
+}
+
+func GetEntriesByIngredient(c *gin.Context) {
+
+}
+
+func UpdateEntry(c *gin.Context) {
+
+}
+
+func UpdateIngredient(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+	entryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": docID})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	c.JSON(http.StatusOK, result.DeletedCount)
+}
